74791b85c8581f657b256fb74d4752d2d9159167
celemar <celemar@pm.me>
2024-04-20 13:37:17 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -81,29 +81,29 @@ function App() {
       </div>
 
       <div className="buttons-container">
-        <button onClick={() => handleClick("7")}>7</button>
-        <button onClick={() => handleClick("8")}>8</button>
-        <button onClick={() => handleClick("9")}>9</button>
-        <button className="delBtn" onClick={() => handleDelete()}>
+        <button type="button" onClick={() => handleClick("7")}>7</button>
+        <button type="button" onClick={() => handleClick("8")}>8</button>
+        <button type="button" onClick={() => handleClick("9")}>9</button>
+        <button type="button" className="delBtn" onClick={() => handleDelete()}>
           DEL
         </button>
-        <button onClick={() => handleClick("4")}>4</button>
-        <button onClick={() => handleClick("5")}>5</button>
-        <button onClick={() => handleClick("6")}>6</button>
-        <button onClick={() => handleClick("+")}>+</button>
-        <button onClick={() => handleClick("1")}>1</button>
-        <button onClick={() => handleClick("2")}>2</button>
-        <button onClick={() => handleClick("3")}>3</button>
-        <button onClick={() => handleClick("-")}>-</button>
-        <button onClick={() => handleClick(".")}>.</button>
-        <button onClick={() => handleClick("0")}>0</button>
-        <button onClick={() => handleClick("/")}>/</button>
-        <button onClick={() => handleClick("*")}>x</button>
+        <button type="button" onClick={() => handleClick("4")}>4</button>
+        <button type="button" onClick={() => handleClick("5")}>5</button>
+        <button type="button" onClick={() => handleClick("6")}>6</button>
+        <button type="button" onClick={() => handleClick("+")}>+</button>
+        <button type="button" onClick={() => handleClick("1")}>1</button>
+        <button type="button" onClick={() => handleClick("2")}>2</button>
+        <button type="button" onClick={() => handleClick("3")}>3</button>
+        <button type="button" onClick={() => handleClick("-")}>-</button>
+        <button type="button" onClick={() => handleClick(".")}>.</button>
+        <button type="button" onClick={() => handleClick("0")}>0</button>
+        <button type="button" onClick={() => handleClick("/")}>/</button>
+        <button type="button" onClick={() => handleClick("*")}>x</button>
 
-        <button className="resetBtn" onClick={() => clearInput()}>
+        <button type="button" className="resetBtn" onClick={() => clearInput()}>
           RESET
         </button>
-        <button className="resultBtn" onClick={() => calculateResult()}>
+        <button type="button" className="resultBtn" onClick={() => calculateResult()}>
           =
         </button>
       </div>
